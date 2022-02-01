2f47d0c558db13f94e67a0440b34db0e98e2ae1e
Brad Bambara <brad.bambara@gmail.com>
2022-02-01 14:00:27 -0500

    Adding a MaPLBuffer class to more easily store bytecode as it is generated.

---
+++ Compiler/Xcode/MaPLCompiler/MaPLBuffer.cpp
@@ -0,0 +1,46 @@
+//
+//  MaPLBuffer.cpp
+//  MaPLCompiler
+//
+//  Created by Brad Bambara on 2/1/22.
+//
+
+#include "MaPLBuffer.h"
+#include <stdlib.h>
+#include <string.h>
+
+MaPLBuffer::MaPLBuffer(size_t bufferCapacity) :
+    _bufferCapacity(bufferCapacity),
+    _byteCount(0)
+{
+    _bytes = (u_int8_t *)malloc(bufferCapacity);
+}
+
+MaPLBuffer::~MaPLBuffer() {
+    if (_bytes) {
+        free(_bytes);
+    }
+}
+
+bool MaPLBuffer::appendBytes(void *bytes, size_t byteSize) {
+    size_t appendedSize = _byteCount + byteSize;
+    if (appendedSize > _bufferCapacity) {
+        u_int8_t *reallocBytes = (u_int8_t *)realloc(_bytes, appendedSize*2);
+        if (!reallocBytes) {
+            return false;
+        }
+        _bytes = reallocBytes;
+        _bufferCapacity = appendedSize*2;
+    }
+    memcpy((u_int8_t *)_bytes+_byteCount, bytes, byteSize);
+    _byteCount += byteSize;
+    return true;
+}
+
+u_int8_t *MaPLBuffer::getBytes() {
+    return _bytes;
+}
+
+size_t MaPLBuffer::getByteCount() {
+    return _byteCount;
+}
---
+++ Compiler/Xcode/MaPLCompiler/MaPLBuffer.h
@@ -0,0 +1,52 @@
+//
+//  MaPLBuffer.h
+//  MaPLCompiler
+//
+//  Created by Brad Bambara on 2/1/22.
+//
+
+#ifndef MaPLBuffer_h
+#define MaPLBuffer_h
+
+#include <stdio.h>
+
+/**
+ * A wrapper for an array of bytes. Allows for easily concatinating bytes while compiling the bytecode.
+ */
+class  MaPLBuffer {
+public:
+    
+    /**
+     * @param bufferCapacity The initial capacity of the buffer. This param can be 0 but a more accurate guess will incur lower cost from reallocating more bytes.
+     */
+    MaPLBuffer(size_t bufferCapacity);
+    ~MaPLBuffer();
+    
+    /**
+     * Appends new bytes onto the buffer.
+     *
+     * @param bytes The new bytes that will be appended onto the buffer.
+     * @param byteSize The number of bytes that should be appended from the `bytes` parameter.
+     *
+     * @return Boolean indicates success or failure of the append operation.
+     */
+    bool appendBytes(void *bytes, size_t byteSize);
+    
+    /**
+     * @return The bytes stored in this buffer.
+     */
+    u_int8_t *getBytes();
+    
+    /**
+     * @return The number of bytes in the buffer returned via the getBytes` method.
+     */
+    size_t getByteCount();
+    
+private:
+    
+    u_int8_t *_bytes;
+    size_t _byteCount;
+    size_t _bufferCapacity;
+};
+
+#endif /* MaPLBuffer_h */
