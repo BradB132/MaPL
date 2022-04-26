470bf2927c32394761b3ad2895b9629ee00f86c8
Brad Bambara <brad.bambara@gmail.com>
2022-04-25 20:01:59 -0400

    Getting a project set up for NoPLGenerator. It doesn't actually do anything yet, but links in all the dependencies.

---
+++ Generator/MaPLGenerator/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  MaPLGenerator
+//
+//  Created by Brad Bambara on 4/25/22.
+//
+
+#include <iostream>
+#include "MaPLCompiler.h"
+#include "MaPLRuntime.h"
+#include <libxml/parser.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // TODO: Decide on CLI for ingesting schema path, MaPL script path, and XML paths.
+    // TODO: Init an EaSL parser.
+    // TODO: Parse with libxml?
+    // TODO: Include MaPLCompiler and MaPLRuntime.
+    // TODO: Create MaPL API file and symlink for the binary product upon build.
+    
+    return 0;
+}
