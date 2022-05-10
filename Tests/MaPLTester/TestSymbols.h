520260a96dc7ea7f5b176ceff63d3d7b99568b62
Brad Bambara <brad.bambara@gmail.com>
2022-05-09 22:57:25 -0400

    Adding a new test script that tests the propagation of generics type information.

--- Tests/MaPLTester/TestSymbols.h
+++ Tests/MaPLTester/TestSymbols.h
@@ -1,15 +1,16 @@
 #ifndef TestSymbols_h
 #define TestSymbols_h
 enum TestSymbols {
-    TestSymbols_ChildType_childProperty = 1,
-    TestSymbols_GLOBAL_childObject = 2,
-    TestSymbols_GLOBAL_falseFunc = 3,
-    TestSymbols_GLOBAL_globalObject = 4,
-    TestSymbols_GLOBAL_print_VARIADIC = 5,
-    TestSymbols_GLOBAL_trueFunc = 6,
-    TestSymbols_Object_floatProperty = 7,
-    TestSymbols_Object_intProperty = 8,
-    TestSymbols_Object_stringProperty = 9,
-    TestSymbols_ParentType_parentProperty = 10,
+    TestSymbols_A_testFunction_X_Y_Z = 1,
+    TestSymbols_ChildType_childProperty = 2,
+    TestSymbols_GLOBAL_childObject = 3,
+    TestSymbols_GLOBAL_falseFunc = 4,
+    TestSymbols_GLOBAL_globalObject = 5,
+    TestSymbols_GLOBAL_print_VARIADIC = 6,
+    TestSymbols_GLOBAL_trueFunc = 7,
+    TestSymbols_Object_floatProperty = 8,
+    TestSymbols_Object_intProperty = 9,
+    TestSymbols_Object_stringProperty = 10,
+    TestSymbols_ParentType_parentProperty = 11,
 };
 #endif /* TestSymbols_h */
