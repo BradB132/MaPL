69ce6697e1b6988a5069c054803a185d9cb222ad
Brad Bambara <brad.bambara@gmail.com>
2022-05-19 08:42:47 -0400

    Simplifying the overly complex CLI argument logic. It's now more user-configurable via flags.

--- Generator/MaPLGenerator/main.cpp
+++ Generator/MaPLGenerator/main.cpp
@@ -12,22 +12,8 @@
 #include "EaSLHandler.h"
 #include <libxml/parser.h>
 
-enum ArgumentExpectation {
-    ArgumentExpectation_None,
-    ArgumentExpectation_GeneratedCodeOutputPath,
-    ArgumentExpectation_DataOutputPath,
-};
-
 void printUsage() {
-    printf("Usage: MaPLGenerator -g generated/code/output/dir -d generated/data/output/dir [paths]\nPaths are expected to contain:\n- At least one '.easl' schema file.\n- At least one '.mapl' script file.\n- Optional '.xml' data file(s).\n");
-}
-
-void confirmNoExpectation(ArgumentExpectation expectation, const char * argv[], int i) {
-    if (expectation != ArgumentExpectation_None) {
-        printf("Missing an output path after flag '%s'.\n", argv[i-1]);
-        printUsage();
-        exit(1);
-    }
+    printf("Usage: MaPLGenerator [paths] or [flags]\nPaths are expected to contain:\n- At least one '.easl' schema file.\n- At least one '.mapl' script file.\n- Optional '.xml' data file(s).\nFlags are prepended with '--' and contain a mapping from name to value. For example, '--myVar=myValue'.");
 }
 
 int main(int argc, const char * argv[]) {
@@ -41,49 +27,22 @@ int main(int argc, const char * argv[]) {
     std::vector<std::filesystem::path> schemaPaths;
     std::vector<std::filesystem::path> scriptPaths;
     std::vector<std::filesystem::path> xmlPaths;
-    std::filesystem::path generatedCodeOutputPath;
-    std::filesystem::path dataOutputPath;
-    std::vector<std::string> flags;
+    std::vector<std::pair<std::string, std::string>> flags;
     
-    ArgumentExpectation expectation = ArgumentExpectation_None;
     for (int i = 1; i < argc; i++) {
         std::string argString = argv[i];
-        if (argString.size() >= 3 && argString[0] == '-' && argString[1] == '-') {
+        if (argString.size() > 2 && argString[0] == '-' && argString[1] == '-') {
             // This arg is a flag (beginning with "--").
-            confirmNoExpectation(expectation, argv, i);
-            flags.push_back(argString.substr(2));
-            continue;
-        }
-        
-        if (argString == "-g") {
-            confirmNoExpectation(expectation, argv, i);
-            expectation = ArgumentExpectation_GeneratedCodeOutputPath;
-            continue;
-        }
-        if (argString == "-d") {
-            confirmNoExpectation(expectation, argv, i);
-            expectation = ArgumentExpectation_DataOutputPath;
+            std::string keyAndValue = argString.substr(2);
+            size_t equalsIndex = keyAndValue.find("=");
+            std::pair<std::string, std::string> flag = { keyAndValue.substr(0,equalsIndex), keyAndValue.substr(equalsIndex+1) };
+            flags.push_back(flag);
             continue;
         }
         
         // This arg is not a flag, assume it's a path.
         std::filesystem::path argPath(argString);
         argPath = std::filesystem::absolute(argPath).lexically_normal();
-        if (expectation != ArgumentExpectation_None) {
-            if (!argPath.extension().empty()) {
-                printf("Path '%s' follows flag '%s' and must point to an output directory.\n", argv[i], argv[i-1]);
-                printUsage();
-                return 1;
-            }
-            if (expectation == ArgumentExpectation_GeneratedCodeOutputPath) {
-                generatedCodeOutputPath = argPath;
-            } else if (expectation == ArgumentExpectation_DataOutputPath) {
-                dataOutputPath = argPath;
-            }
-            expectation = ArgumentExpectation_None;
-            continue;
-        }
-        
         std::string pathExtension = argPath.extension();
         std::transform(pathExtension.begin(), pathExtension.end(), pathExtension.begin(), [](unsigned char c){
             return std::tolower(c);
@@ -101,7 +60,6 @@ int main(int argc, const char * argv[]) {
             return 1;
         }
     }
-    confirmNoExpectation(expectation, argv, argc);
     if (schemaPaths.empty()) {
         printf("Expected at least one '.easl' schema file.\n");
         printUsage();
@@ -109,16 +67,6 @@ int main(int argc, const char * argv[]) {
     }
     if (scriptPaths.empty()) {
         printf("Expected at least one '.mapl' script file.\n");
-        printUsage();
-        return 1;
-    }
-    if (generatedCodeOutputPath.empty()) {
-        printf("Expected an output directory for generated code (preceded by a -g flag).\n");
-        printUsage();
-        return 1;
-    }
-    if (!xmlPaths.empty() && dataOutputPath.empty()) {
-        printf("Expected an output directory for data (preceded by a -d flag).\n");
         printUsage();
         return 1;
     }
