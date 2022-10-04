89fbcb56c5894e19ab3d19464193b4722f42e97f
Brad Bambara <brad.bambara@gmail.com>
2022-10-04 14:24:53 -0400

    Clarifying commandline output from MaPLGenerator.

--- Generator/MaPLGenerator/main.cpp
+++ Generator/MaPLGenerator/main.cpp
@@ -15,7 +15,7 @@
 #include <libxml/parser.h>
 
 void printUsage() {
-    fprintf(stderr, "Usage: MaPLGenerator [paths] and optional [flags]\n  Paths are expected to contain:\n    - At least one '.easl' schema file.\n    - At least one '.mapl' script file.\n    - Optional '.xml' data file(s).\n  Flags are prepended with '--' and contain a mapping from name to value. For example, '--myVar=myValue'.\n");
+    fprintf(stderr, "Usage: MaPLGenerator [paths] [flags (optional)]\n  Paths are expected to contain:\n    - At least one '.easl' schema file.\n    - At least one '.mapl' script file.\n    - Optional '.xml' data file(s).\n  Flags are prepended with '--' and contain a mapping from name to value. For example, '--myVar=myValue'.\n");
 }
 
 int main(int argc, const char * argv[]) {
@@ -56,7 +56,7 @@ int main(int argc, const char * argv[]) {
         } else if (pathExtension == ".xml") {
             xmlPaths.push_back(argPath);
         } else {
-            fprintf(stderr, "File '%s' is unexpected type.\n", argv[i]);
+            fprintf(stderr, "File '%s' is unexpected file type.\n", argv[i]);
             printUsage();
             return 1;
         }
