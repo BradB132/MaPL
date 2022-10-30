0d1c6b881d53b80aa8fc0d8b20b0c40ec3ba3e35
Brad Bambara <brad.bambara@gmail.com>
2022-10-30 14:01:08 -0400

    Updating the ANTLR4 dependency to v4.11.1. This presents a temporary challenge because the Xcode project is broken in this revision, so needs to be manually updated for the project to work. I will attempt to submit a pull request to fix this later. The upside is that the ANTLR command line interface has been streamlined, which simplifies the generate_parser scripts.

--- Generator/EaSL/generate_parser.sh
+++ Generator/EaSL/generate_parser.sh
@@ -3,54 +3,22 @@
 # Move into the directory of this script.
 cd "$(dirname "$0")"
 
-# Make sure we know where the ANTLR jar is.
-ANTLR_PATH="/Applications/antlr-4.9.3-complete.jar"
-if [ ! -f "${ANTLR_PATH}" ]; then
-    echo "Couldn't find ANTLR. This script assumes your antlr.jar is located at '${ANTLR_PATH}'. If not, edit this script to fix the path."
-    return 1
-fi
-
-# Make sure Java is installed.
-if [ $(which java) == "" ]; then
-    echo "Couldn't find Java. Make sure 'java' is installed."
-    return 1
-fi
-
-# Make sure JAVA_HOME environment var is set.
-if [ "${JAVA_HOME}" == "" ]; then
-    export JAVA_HOME=$(/usr/libexec/java_home)
-fi
-
-# Make sure ANTLR_PATH is in CLASSPATH.
-if [[ "${CLASSPATH}" != *"${ANTLR_PATH}"* ]]; then
-    export CLASSPATH=".:${ANTLR_PATH}:${CLASSPATH}"
-fi
+# Make sure ANTLR4 is installed.
+export PATH=$PATH:~/Library/Python/3.9/bin
+python3 -m pip install antlr4-tools
 
 ARG1=$(echo "${1}" | awk '{print tolower($0)}')
 if [ "${ARG1}" == "c++" ]; then
     mkdir -p "./generated_c++"
 
     echo "Generating C++ code for the parser..."
-    java -jar "${ANTLR_PATH}" EaSL.g4 -o ./generated_c++ -Dlanguage=Cpp -no-listener
+    antlr4 ./EaSL.g4 -o ./generated_c++ -Dlanguage=Cpp -no-listener
     
     # Clean up extraneous files.
     rm ./generated_c++/*.interp
     rm ./generated_c++/*.tokens
     
 else
-    mkdir -p "./generated_java"
-    
-    echo "Generating Java code for the parser..."
-    java -jar "${ANTLR_PATH}" EaSL.g4 -o ./generated_java
-    
-    echo "Compiling parser code..."
-    cd ./generated_java
-    javac *.java
-    
-    # Clean up extraneous files.
-    rm *.interp
-    rm *.tokens
-    
     echo "Opening parser output in GUI..."
-    java org.antlr.v4.gui.TestRig EaSL schema -gui ../exampleSchema.easl
+    antlr4-parse ./EaSL.g4 schema -gui ./exampleSchema.easl
 fi
